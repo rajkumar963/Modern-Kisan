d8a8c29c70453b64f05536529a25472f0e6a774e
Aryan <arynch24@gmail.com>
2025-03-29 13:01:45 +0530

    Add Chatbot component to Index page

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -14,6 +14,7 @@ import GovernmentGrants from '@/components/GovernmentGrants';
 import BlogSection from '@/components/BlogSection';
 import Marketplace from '@/components/Marketplace';
 import Footer from '@/components/Footer';
+import Chatbot from '@/components/ui/ChatBot';
 
 const Index = () => {
   // Animation for elements when they come into view
@@ -56,6 +57,7 @@ const Index = () => {
         <DraggableCourseCards />
         <Testimonials />
         <Partners />
+        <Chatbot />
       </main>
       <Footer />
     </div>
